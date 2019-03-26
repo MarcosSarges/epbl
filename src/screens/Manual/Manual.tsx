99abf0e8af010f9cde12cece6201733e17093dcf
Marcos Sarges <dev.marcos.sarges@gmail.com>
2019-03-25 23:37:18 -0300

    transformando o manual em um componente sem estado

--- src/screens/Manual/Manual.tsx
+++ src/screens/Manual/Manual.tsx
@@ -1,55 +1,33 @@
-import React, { Component } from 'react'
-import { View, FlatList, TouchableOpacity } from 'react-native'
+import React from 'react'
+import { FlatList } from 'react-native'
 import { NavigationScreenProps } from 'react-navigation';
+import { Card, CardItem, Text } from 'native-base';
 import Container from '../../components/Container';
-import Header from '../../components/Header';
-import { Card, CardItem, Text, Body } from 'native-base';
-import { Input, Icon } from 'react-native-elements'
 import Conteudo from './Conteudo';
 
 type Props = {} & NavigationScreenProps
 
-class Manual extends Component<Props> {
+interface Item {
+    titulo: string, conteudo: string, key: string
+}
 
-    state = {
-        data: Conteudo,
-        dataOrigem: Conteudo
-    }
-
-    filter = (input: string) => {
-        const { data, dataOrigem } = this.state;
-
-        if (input.length == 0) {
-            this.setState({ data: dataOrigem });
-        } else {
-            this.setState({
-                data: data.filter((el) => el.titulo.toLowerCase().includes(input.toLowerCase()) && true)
-            })
-        }
-    }
-
-    renderItem = (item: { titulo: string, conteudo: string, key: string }) =>
+const Manual = (props: Props) => {
+    const renderItem = (item: Item) =>
         <Card key={item.key}>
-            <CardItem
-                header
-                bordered
-                button
-                onPress={() => this.props.navigation.navigate('DetalhesManual', item)}
+            <CardItem header bordered button
+                onPress={() => props.navigation.navigate('DetalhesManual', item)}
                 style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+
                 <Text>{item.titulo}</Text>
             </CardItem>
         </Card >
 
-    render() {
-        return (
-            <Container title='Manual do PBL' navigation={this.props.navigation}>
-                <FlatList
-                    data={this.state.data}
-                    renderItem={({ item }) => this.renderItem(item)}
-                />
-            </Container>
-        );
-    }
+    return (
+        <Container title='Manual do PBL' navigation={props.navigation}>
+            <FlatList data={Conteudo} renderItem={({ item }) => renderItem(item)} />
+        </Container>
+    );
+
 }
 
 export default Manual;
